50d207d388f90d38ea7bee87b2f38fab216c1da7
XuJian <xujian@gpdi.com>
2021-08-10 17:44:30 +0800

    feat：首页样式构建

---
+++ src/components/subtitle/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="subtitle-wrap">
+		<view class="icon" :style="{ backgroundColor: iconColor }"></view>
+		<text class="text">{{ text }}</text>
+	</view>
+</template>
+
+<script>
+export default {
+	props: {
+		iconColor: {
+			type: String,
+			default: "#ff9115"
+		},
+		text: {
+			type: String,
+			default: ""
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.subtitle-wrap {
+	height: 50rpx;
+	display: flex;
+	align-items: center;
+	padding: 0 20rpx;
+
+	.icon {
+		width: 6rpx;
+		height: 16rpx;
+    border-radius: 3rpx;
+	}
+  .text{
+    color: #666666;
+    font-size: 24rpx;
+    margin-left: 11rpx;
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,17 +5,64 @@
 			src="/static/images/background_index.jpg"
 			mode="aspectFill"
 			fade-show="false"
-			draggable="false"
+			:draggable="false"
 		></image>
 		<view class="index-container">
-			<text class="welcome">欢迎回来，{{ username }}！</text>
-			<van-tag type="success">管理员</van-tag>
+			<view class="welcome">
+				<text class="text">欢迎回来，{{ username }}！</text>
+				<van-tag type="success">管理员</van-tag>
+			</view>
+			<view class="overview-wrap">
+				<subtitle text="七日订单情况"></subtitle>
+				<van-row>
+					<van-col span="6">
+						<view class="content">
+							<text class="value">2</text>
+							<text class="desc">未接单</text>
+						</view>
+					</van-col>
+					<van-col span="6">
+						<view class="content">
+							<text class="value">3</text>
+							<text class="desc">已接单</text>
+						</view>
+					</van-col>
+					<van-col span="6">
+						<view class="content">
+							<text class="value">38</text>
+							<text class="desc">已完成</text>
+						</view>
+					</van-col>
+					<van-col span="6">
+						<view class="content">
+							<text class="value">6</text>
+							<text class="desc">已取消</text>
+						</view>
+					</van-col>
+				</van-row>
+			</view>
+			<view class="icon-wrap">
+				<subtitle text="工单功能"></subtitle>
+				<view class="icon-panel">
+					<view class="icon-item">
+						<view class="icon" >
+							<image src="/static/icon/index/icon_work.png" mode="aspectFit" />
+						</view>
+						<text class="text">工单管理</text>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+import subtitle from "@/components/subtitle/index.vue";
+
 export default {
+	components: {
+		subtitle
+	},
 	data() {
 		return {
 			username: "李立强"
@@ -43,10 +90,73 @@ export default {
 		position: relative;
 		z-index: 5;
 		padding: 129rpx 30rpx 0 30rpx;
-
 		.welcome {
-			font-size: 40rpx;
-			color: #ffffff;
+			.text {
+				font-size: 40rpx;
+				color: #ffffff;
+			}
+		}
+		.overview-wrap {
+			margin-top: 52rpx;
+			width: 100%;
+			background-color: #ffffff;
+			border-radius: 18rpx;
+			box-shadow: 0 0 10rpx #cccccc;
+			.content {
+				height: 180rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				flex-direction: column;
+				.desc {
+					color: #999999;
+					font-size: 20rpx;
+				}
+				.value {
+					color: #ff9216;
+					font-size: 40rpx;
+					margin-bottom: 16rpx;
+					white-space: nowrap;
+				}
+			}
+		}
+
+		.icon-wrap {
+			margin-top: 20rpx;
+			width: 100%;
+			background-color: #ffffff;
+			border-radius: 18rpx;
+			box-shadow: 0 0 10rpx #cccccc;
+			.icon-panel {
+				display: flex;
+				flex-wrap: wrap;
+				.icon-item {
+					height: 144rpx;
+					width: 25%;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					.icon {
+						width: 80rpx;
+						height: 80rpx;
+						background-color: red;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						border-radius: 12rpx;
+            margin-bottom: 14rpx;
+						image {
+							width: 36rpx;
+							height: 36rpx;
+						}
+					}
+					.text {
+						color: #666666;
+						font-size: 20rpx;
+					}
+				}
+			}
 		}
 	}
 }
